0effa4edf9780e5f4eac3782b92bd673570a8028
Федулов Олег Викторович (4103815) <ofedulov@dev.vtb.ru>
2025-09-09 07:32:01 +0300

    space

--- bonus.py
+++ bonus.py
@@ -3,7 +3,6 @@ import random
 import os
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, BONUS_WIDTH, BONUS_HEIGHT
 
-
 class Bonus:
     def __init__(self, bonus_type):
         self.type = bonus_type
